b7f4015c9e2dc3cb38472b391f03b21a10d0d096
stellar-server-0187 <jaimedavid01@yahoo.com>
2020-10-10 06:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 function dwarfRollCall(dwarves) {
+  if (dwarves.length > 0);
+let output = [];
+for (var i = 0; i < dwarves.length; i++){
+  output += (i + 1) + ". " + dwarves[i] + " ";}
+  return output;
 }
-
 function summonCaptainPlanet(planeteerCalls){
-}
+  var output = [];
+   output += planeteerCalls.push(`${planeteerCalls}!`.toUpperCase());
+} return output;
+
 
 function longPlaneteerCalls(words) {
 }
